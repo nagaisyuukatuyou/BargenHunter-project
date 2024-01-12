177d9fc22acf2d521330cdc5d00e41443a1f146d
Hiro0803-HK <kd1339439@st.kobedenshi.ac.jp>
2024-01-12 10:47:26 +0900

    2024/01/12

--- database/seeders/SupermarketDetailsTableSeeder.php
+++ database/seeders/SupermarketDetailsTableSeeder.php
@@ -15,9 +15,9 @@ class SupermarketDetailsTableSeeder extends Seeder
     {
         //
         $supermarket_details = [
-            ['supermarket_id' => '1', 'nearest_station' => '神戸駅', 'web_site' => 'www.kohyo.co.jp', 'phone_number' => '078-341-5040', 'address' => '兵庫県神戸市中央区相生町3-1-2', 'close_time' => '23:00', 'open_time' => '7:00'],
+            ['supermarket_id' => '1', 'nearest_station' => '神戸駅', 'web_site' => 'https://www.kohyo.co.jp/', 'phone_number' => '078-341-5040', 'address' => '兵庫県神戸市中央区相生町3-1-2', 'close_time' => '23:00', 'open_time' => '7:00'],
             ['supermarket_id' => '2', 'nearest_station' => '中央市場駅', 'web_site' => 'https://www.supermaruhachi.co.jp/shop/瓜生堂店-2-3/', 'phone_number' => '078-686-0868', 'address' => '兵庫県神戸市兵庫区西仲町2-36', 'close_time' => '21:45', 'open_time' => '09:00'],
-            ['supermarket_id' => '3', 'nearest_station' => '三宮駅', 'web_site' => 'www.daiei.co.jp/stores/d0622/', 'phone_number' => '078-291-0077', 'address' => '兵庫県神戸市中央区雲井通6丁目1-15', 'close_time' => '23:00', 'open_time' => '9:00'],
+            ['supermarket_id' => '3', 'nearest_station' => '三宮駅', 'web_site' => 'https://www.daiei.co.jp/stores/d0622/', 'phone_number' => '078-291-0077', 'address' => '兵庫県神戸市中央区雲井通6丁目1-15', 'close_time' => '23:00', 'open_time' => '9:00'],
             ['supermarket_id' => '4', 'nearest_station' => '神戸駅', 'web_site' => 'http://www.lifecorp.jp/store/kinki/det_kinki.html?region=kinki&base_recordno=145', 'phone_number' => '078-371-6511', 'address' => '兵庫県神戸市中央区中町通3丁目2-15', 'close_time' => '00:00', 'open_time' => '9:30'],
         ];
 
--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -20,12 +20,14 @@
   </div>
   <hr>
   <br>
+  <div style="margin: 0 auto; width: 1050px;"><!--追加-->
   <div class="ice-cream-photo">
     <img src="{{ asset('images/' . $category->title . '/' . $img->image) }}" width="300" height="300">
   </div>
   <div class="shop-list">
   @foreach($results as $result)
     <div class="shop-list-item">
+      <img src="{{ asset('images/店画像/'. '/' . $result->s_image) }}" width="100" height="100">
       <div class="shop-list-item-info">
         <div class="shop-list-item-name">スーパー名：{{ $result->s_name }}</div>
         <div class="shop-list-item-price">価格：{{ $result->price }}円</div>
@@ -33,10 +35,12 @@
         <div class="shop-list-item-hours">開店時間：{{ $result->open_time }}</div>
         <div class="shop-list-item-hours">開店時間：{{ $result->close_time }}</div>
         <div class="shop-list-item-address">電話番号：{{ $result->phone_number }}</div>
+        <div class="shop-list-item-address">ウェブサイト：<a href="{{ $result->web_site }}">{{ $result->web_site }}</a></div>
         <br>
     </div>
     </div>
     @endforeach
+  </div>
     </div>
 </body>
 </html>
