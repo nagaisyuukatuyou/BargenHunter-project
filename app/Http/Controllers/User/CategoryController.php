31cd57988540b356543a78cc0d73656a37b76f5f
nagaihiroshi414 <kd1323900@st.kobedenshi.ac.jp>
2024-01-16 14:31:53 +0900

    24/01/16

--- app/Http/Controllers/User/CategoryController.php
+++ app/Http/Controllers/User/CategoryController.php
@@ -19,7 +19,7 @@ class CategoryController extends Controller
     public function index(): View
     {
         //カテゴリ一覧全件取得
-        $categories = Category::all();
+        $categories = Category::query()->get();//all();
         $category_id = Category::select('id')->get();
         $images = Category::select('image')->get();
 
@@ -39,10 +39,11 @@ class CategoryController extends Controller
     }
 
     public function getProducts(string $category_id): View
-    {
-        //商品テーブルの指定のカテゴリIDと一致するカラムを全件取得
+
+    {   
+        //商品テーブルの指定のカテゴリIDと一致するデータを全件取得
         $products = Product::select('*')->where('category_id', $category_id)->get();
-        //指定のカテゴリIDとカテゴリテーブルのIDが一致するカラムを取得する
+        //指定のカテゴリIDとカテゴリテーブルのIDが一致するデータを取得する
         $category = $this->getCategory($category_id);
 
         return view('products', [
@@ -54,33 +55,75 @@ class CategoryController extends Controller
     public function getResults(string $product_id)
     {
         //スーパーマーケットテーブル、価格テーブル、スーパー詳細テーブルを結合し、以下のIDと一致するカラムを全件取得
-        $results = DB::table('prices')
-            ->join('supermarkets', 'prices.supermarket_id', '=', 'supermarkets.id')
-            ->join('supermarket_details', 'supermarkets.id', '=', 'supermarket_details.supermarket_id')
-            ->select('prices.*', 'supermarkets.*', 'supermarket_details.*')
-            ->where('prices.product_id', '=', $product_id)
-            ->get();
 
-        return $results;
+        /*$sql = DB::table('prices')
+        ->join('supermarkets', 'prices.supermarket_id', '=', 'supermarkets.id')
+        ->join('supermarket_details', 'supermarkets.id', '=', 'supermarket_details.supermarket_id')
+        ->select('prices.*', 'supermarkets.*', 'supermarket_details.*')
+        ->where('prices.product_id', '=', $product_id)
+        ->orderBy('price');*/
+
+        $sql = Price::query()
+        ->join('supermarkets', 'prices.supermarket_id', '=', 'supermarkets.id')
+        ->join('supermarket_details', 'supermarkets.id', '=', 'supermarket_details.supermarket_id')
+        ->where('prices.product_id', '=', $product_id)
+        ->orderBy('price');
+
+
+        $results = $sql->get();
+        $minresult = $sql->limit(1)->first();
+
+        return [$results, $minresult];
     }
 
     //カテゴリテーブルと商品テーブルを操作するため、変数は2つ設定する
-    public function getPrices(string $category_id, string $product_id): View
-    {
-        //商品テーブルに格納されている画像名をproduct_idと一致したものを取得する
-        $img = Product::select('image')->where('id', $product_id)->first();
-        $results = $this->getResults($product_id);
-        //カテゴリIDと一致するカラムの情報を取得
-        $category = $this->getCategory($category_id);
-        $count = count($results);
 
+    public function getPrices(string $category_id, string $product_id, Request $request): View
+    {   
+        //商品テーブルに格納されている画像名をproduct_idと一致したものを取得する
+        $img_name = Product::select('image', 'product_name')->where('id', $product_id)->first();
+
+        $keyword = $request->input('keyword');
+
+        $sql = Price::query()
+        ->join('supermarkets', 'prices.supermarket_id', '=', 'supermarkets.id')
+        ->join('supermarket_details', 'supermarkets.id', '=', 'supermarket_details.supermarket_id')
+        ->where('prices.product_id', '=', $product_id)
+        ->orderBy('price');
+
+        $product = $sql->first();
+        $results = $this->getResults($product_id);
+
+        $Results = $results[0];
+
+          //カテゴリIDと一致するカラムの情報を取得
+          $category = $this->getCategory($category_id);
+          $Minresult = $results[1];
+          $count = count($Results);
+
+        if(!empty($keyword)) {
+            $sql->where('s_name', 'like', "%{$keyword}%");
+            $Results = $sql->get();
+            $count = count($Results);
+            $keyword = $keyword;
+            
+        }
+        else{
+            $keyword = '';
+        }
+        
         return view('results', [
-            'img' => $img,
-            'results' => $results,
-            'count' => $count,
-            'category' => $category,
+           'img_name' => $img_name,
+           'results' => $Results,
+           'minresult' => $Minresult,
+           'count' => $count,
+           'category' => $category,
+           'product' => $product,
+           'keyword' => $keyword,
         ]);
 
     }
+
+    //public function 
 
 }
