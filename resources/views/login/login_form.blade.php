85448dee7e22af788b3e2ab4329d9c4ea3c42d4a
Hiro0803-HK <kd1339439@st.kobedenshi.ac.jp>
2024-01-24 10:15:18 +0900

    2024/01/24

--- resources/views/login/login_form.blade.php
+++ resources/views/login/login_form.blade.php
@@ -33,17 +33,17 @@
             @endif
 
             <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com">
+                <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com" autocomplete="off">
                 <label for="floatingInput">Email address</label>
             </div>
             <div class="form-floating">
                 <input type="password" class="form-control" id="floatingPassword" name="password"
-                    placeholder="Password">
+                    placeholder="Password" autocomplete="off">
                 <label for="floatingPassword">Password</label>
             </div>
 
             <div class="form-check text-start my-3">
-                <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+                <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" autocomplete="off">
                 <label class="form-check-label" for="flexCheckDefault">
                     Remember me
                 </label>
